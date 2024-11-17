a9d47e9ea390d703996b7ddaef74d4ddf585599d
EstefSpace <estef.space@gmail.com>
2024-11-16 22:18:35 -0600

    cambio de estrucutra, imagenes fix

---
+++ assets/javascript/typed-index.js
@@ -0,0 +1,11 @@
+//index
+new Typed('#typed-index', {
+    strings: [
+      'un estudiante',
+      'un programador',
+      'un apasionado a la tecnologia',
+      'Estefan',
+    ],
+    typeSpeed: 80,
+    loop: true,
+  });
---
+++ assets/javascript/typed-portafolio.js
@@ -0,0 +1,8 @@
+
+      //portafolio
+
+      new Typed('#typed-portafolio', {
+        strings: ['portafolio digital', 'rincon digital', 'lugar seguro'],
+        typeSpeed: 80,
+        loop: true,
+      });
