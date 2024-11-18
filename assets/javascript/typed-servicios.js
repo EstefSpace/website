5a3a5d5e917c8d5e6e404f2028279d6e8857ffd4
EstefSpace <estef.space@gmail.com>
2024-11-17 21:52:03 -0600

    mega update

---
+++ assets/javascript/typed-servicios.js
@@ -0,0 +1,8 @@
+
+      //servicios
+
+      new Typed('#typed-servicios', {
+        strings: ["servicios, para ti.", "servicios, para el mundo.", ],
+        typeSpeed: 50,
+        loop: true,
+      });
