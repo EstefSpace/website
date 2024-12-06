0fd301bcf7f07adf60c150b52379495d2b08f479
EstefSpace <estef.space@gmail.com>
2024-12-05 21:42:58 -0600

    pequeño cambio en portafolio

--- assets/javascript/typed-portafolio.js
+++
@@ -1,8 +0,0 @@
-
-      //portafolio
-
-      new Typed('#typed-portafolio', {
-        strings: ['portafolio digital', 'rincon digital', 'lugar seguro'],
-        typeSpeed: 80,
-        loop: true,
-      });
